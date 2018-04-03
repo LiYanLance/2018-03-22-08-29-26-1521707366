2161aadcdabd66147c8539e25c224c06ca38e7e1
LiYanLance <leeyanly@163.com>
2018-04-03 09:03:37 +0800

    InverseReduceTest passed

--- src/main/java/com/thoughtworks/collection/InverseReduce.java
+++ src/main/java/com/thoughtworks/collection/InverseReduce.java
@@ -2,20 +2,28 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class InverseReduce {
 
     private Random random = new Random();
 
-    public InverseReduce() {}
+    public InverseReduce() {
+    }
 
     public InverseReduce(Random random) {
         this.random = random;
     }
 
     public List<Integer> divideToSmaller(int number) {
-        throw new NotImplementedException();
+        int minus = random.nextInt(3);
+        return IntStream.range(0, number).boxed()
+                .sorted((a, b) -> -a.compareTo(b))
+                .filter(num -> (number - num) % minus == 0)
+                .collect(Collectors.toList());
     }
 }
